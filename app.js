28225d0d6e2a41b7fab9ad79ae0d8956adfb3ff2
Víctor <victorsobrinos@gmail.com>
2022-08-29 18:52:07 +0200

    login and register routes created

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', require('./routes/index'))
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
 
+//Routes
+app.use('/', require('./routes/index'))
+app.use('/users', require('./routes/user'))
