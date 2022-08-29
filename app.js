4f75a3b37ae0a614b61d3e437f80fe489ccfb5e2
Víctor <victorsobrinos@gmail.com>
2022-08-29 18:40:23 +0200

    first routes created and tested ok

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
+
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
+
